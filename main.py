c99d8b51eda757ce7e29a734faa660bb998ad479
brandonviaje <viajebrandon@gmail.com>
2026-02-04 16:33:10 -0500

    refactor: setup event loop

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
-from account_reader import read_bank_accounts
+import transactions
 
 if __name__ == "__main__":
     file_path = "bank_accounts.txt"
-    accounts = read_bank_accounts(file_path)
+    session_active = True
+    accounts = {}
 
-    print("Accounts:")
-    for acc_num in accounts.keys():
-        print(accounts[acc_num])
+    # start event loop
+    while session_active:
+        code = input("Enter transaction code: ").strip().lower()
+
+        # handle login/logout
+        if code == "login":
+            accounts = transactions.handle_login(file_path)
+        elif code == "logout":
+            transactions.handle_logout(file_path)
+            session_active = False  # end session
+        else:
+            # must be logged in first
+            if not transactions.session_active:
+                print("You must login first.")
+                continue
+
+            # add ur function here when u guys are done adding ur logic (make sure u write ur function in transactions.py)
+            if code == "withdrawal":
+                pass
+            elif code == "deposit":
+                pass
+            elif code == "transfer":
+                pass
+            elif code == "paybill":
+                pass
+            elif code == "create":
+                pass
+            elif code == "delete":
+                pass
+            elif code == "disable":
+                pass
+            elif code == "changeplan":
+                pass
+            else:
+                print("Invalid transaction code")
